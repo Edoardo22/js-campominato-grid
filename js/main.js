f809f4fdda61fe7b85e5fc56ef3e0ba5a7fc1182
Edoardo22 <vanaliedoardo@gmail.com>
2022-05-30 18:18:21 +0200

    es finito

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 const containerGriglia = document.getElementById("containerGriglia");
-function generaNumeroCasuale(min, max) {
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
-}
 
 for (let i = 0; i < 64; i++) {
     let cella = document.createElement("div");
     cella.classList.add("cella");
-    cella.innerHTML = `<span>${generaNumeroCasuale(1, 100)}</span>`;
+    cella.innerHTML = `<span class="text-center">${i + 1}</span>`;
     containerGriglia.append(cella);
     cella.addEventListener("click", function () {
 
@@ -16,7 +12,7 @@ for (let i = 0; i < 64; i++) {
 
 
     });
-    //se clicco di nuovo sulla cella la sua colore diventa bianco
+
     cella.addEventListener("dblclick", function () {
 
         cella.style.backgroundColor = "#0dcaf0";
